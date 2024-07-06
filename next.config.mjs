7373d356a63fb55be22154619d59b61c445e9fa8
UniLeonid <admin@smashup.ru>
2024-07-06 18:46:11 +0300

    [week5] Fix Github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/FrontEnd-Course",
   images: {
     remotePatterns: [
       {
@@ -9,6 +11,7 @@ const nextConfig = {
         pathname: '/**'
       }
     ],
+    unoptimized: true,
   },
 };
 
